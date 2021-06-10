2fadaea0b6751c66b125ab94b1e89fe9350277ca
EugeneStudent <69599396+EugeneStudent@users.noreply.github.com>
2021-06-10 18:20:56 +0300

    //

--- For_Test.cpp
+++ For_Test.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+﻿/*#include <iostream>
 
 class A
 {
@@ -77,4 +77,129 @@ int main()
 const double& First::foo(Second other_class)
 {
 	return p_a + other_class.p_b + other_class.p_c;
+}*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//#define WAY //пример использования условной компиляции 
+#ifndef WAY
+class Image
+{
+
+public:
+	Image()
+	{
+		counter++;
+	}
+	static int counter;
+	class Pixel
+	{
+		int r, g, b;
+	public:
+		Pixel(int r, int g, int b)
+		{
+			this->r = r;
+			this->g = g;
+			this->b = b;
+		}
+
+		string GetInfoPixel()
+		{
+			return "R= " + to_string(r) + " G= " + to_string(g) + " B= " + to_string(b);
+		}
+	};
+	static const int a = 3;
+	int arr[a];
+	static int GetID()
+	{
+		return counter;
+	}
+private:
+	Pixel arr_pixels[5]
+	{
+		Pixel(43,67,14),
+		Pixel(65,87,114),
+		Pixel(243,186,17),
+		Pixel(203,147,117),
+		Pixel(46,88,115)
+	};
+};
+int Image::counter = 0; //статическую переменню класса должны инициализировать сразу
+						//за пределами определения класса
+
+const int Max = 5;
+
+void centimize(double* ptrd)
+{
+	cout << ptrd++ << endl;
+	for (int i = 0; i < Max - 1; i++)
+		*ptrd++ *= 2.54;
 }
+
+void main()
+{
+	double arr[Max] = { 10.0, 43.1, 95.9, 58.7, 87.3 };
+	cout << arr << endl;
+	centimize(arr);
+	for (int i = 0; i < Max; i++)
+		cout << *(arr + i) << "\n";
+	//	Image::Pixel pixel(199, 65, 255); //мы можем создать объект класса, который определен
+	//	//вложенным в другом классе
+	//	cout << pixel.GetInfoPixel() << endl;
+	//
+	//	cout << Image::counter << endl;//мы можем обращаться к статическим членам класса без объекта
+
+}
+#else
+class Base // Базовый класс
+{
+	int x = 100;
+public:
+	void show() // Обычная функция
+	{
+		cout << "Base\n";
+	}
+	int GetX()
+	{
+		return x;
+	}
+};
+//////////////////////////////////////////////
+class Derv1 : public Base // Производный класс 1
+{
+public:
+	void show()
+	{
+		cout << "Derv1\n";
+	}
+};
+//////////////////////////////////////////////
+class Derv2 : public Base // Производный класс 2
+{
+public:
+	void show()
+	{
+		cout << "Derv2\n";
+	}
+};
+//////////////////////////////////////////////
+int main()
+{
+	Derv1 dv1; // Объект производного класса 1
+	Derv2 dv2; // Объект производного класса 2
+	Base* ptr; // Указатель на базовый класс
+	//Base::GetX();
+	ptr = &dv1; // Адрес dv1 занести в указатель
+	ptr->show(); // Выполнить show()
+	ptr = &dv2; // Адрес dv2 занести в указатель
+	ptr->show(); // Выполнить show()
+	return 0;
+}
+#endif
+
+
+
+
+
